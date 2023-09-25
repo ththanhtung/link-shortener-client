8fea07a63dbc91319e897442d6d281c2b47531f7
ththanhtung <nguyenththanhtung317@gmail.com>
2023-09-25 16:21:07 +0700

    refactor: add check error

--- src/features/home/page/Main/index.tsx
+++ src/features/home/page/Main/index.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useEffect, useState } from 'react';
 import { useInput } from '../../../../hooks/useInput';
 import URLApi from '../../../../api/URLApi';
+import { AxiosError } from 'axios';
 
 interface LinksRes {
   metadata: {
@@ -13,7 +14,7 @@ interface LinksRes {
 
 const Main: React.FC = () => {
   const [fullLink, resetfullLink, fullLinkAttrs] = useInput('fullLink', '');
-  const [shortLink, setShortLink] = useState<string|null>()
+  const [shortLink, setShortLink] = useState<string | null>();
   const [links, setLinks] = useState<
     | {
         url_shorten_link: string;
@@ -29,7 +30,10 @@ const Main: React.FC = () => {
 
         setLinks(links.metadata.links);
       } catch (error) {
-        console.log(error);
+        if (error instanceof AxiosError) {
+          const errors = JSON.stringify(error.response?.data.errors[0].message);
+          alert(errors);
+        }
       }
     }
 
@@ -38,34 +42,50 @@ const Main: React.FC = () => {
 
   const handleCreateNewLink = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const resp = await URLApi.CreateNewShortenLink({
-      fullLink: fullLink
-    })
 
-    const newShortLink = resp.data as {
-      metadata: {
-        link: {
-          url_shorten_link: string;
-          url_full_link: string;
+    try {
+      const resp = await URLApi.CreateNewShortenLink({
+        fullLink: fullLink,
+      });
+
+      const newShortLink = resp.data as {
+        metadata: {
+          link: {
+            url_shorten_link: string;
+            url_full_link: string;
+          };
         };
       };
-    };
 
-    setShortLink(newShortLink.metadata.link.url_shorten_link)
+      setShortLink(newShortLink.metadata.link.url_shorten_link);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const errors = JSON.stringify(error.response?.data.errors[0].message);
+        alert(errors);
+      }
+    }
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <form action="" onSubmit={handleCreateNewLink}>
         <div className="input-field">
           <label htmlFor="link">link</label>
-          <input type="text" id="link" {...fullLinkAttrs} placeholder='enter the link you want to shorten' />
+          <input
+            type="text"
+            id="link"
+            {...fullLinkAttrs}
+            placeholder="enter the link you want to shorten"
+          />
         </div>
         <button type="submit">create new shorten link</button>
       </form>
       <div className="current-short-link">
         <span>your current short link is: </span>
-        <a href={`${import.meta.env.VITE_URL}/url/${shortLink}`} target='_blank'>
+        <a
+          href={`${import.meta.env.VITE_URL}/url/${shortLink}`}
+          target="_blank"
+        >
           {shortLink}
         </a>
       </div>
